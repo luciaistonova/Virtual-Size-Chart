d4fdaf992ff3c229cdab7b07c6ef774f66415c68
Lucia Istonova <67709782+luciaistonova@users.noreply.github.com>
2023-04-20 18:37:34 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,93 +1,77 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
+import React from 'react';
 import CameraComponent from './CameraComponent';
+import SizeChartApp from './SizeChartApp';
+import './App.css';
 
-function App() {
-  const [clientSize, setClientSize] = useState('');
-  const [imageUri, setImageUri] = useState(null);
-
-  useEffect(() => {
-    function handleResize() {
-      const screenWidth = window.innerWidth;
-      if (screenWidth <= 768) {
-        setClientSize('Small');
-      } else if (screenWidth > 768 && screenWidth < 992) {
-        setClientSize('Medium');
-      } else {
-        setClientSize('Large');
-      }
-    }
-
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  function handleRectangleClick(event) {
-    // Get the id of the clicked rectangle image
-    const id = event.target.id;
-
-    // Do something with the clicked rectangle image (e.g. navigate to a new page)
-    console.log(`You clicked on rectangle image ${id}`);
-  }
-
-  function handleImageClick(event) {
-    // Get the source of the clicked image and set it as the selected image URI
-    setImageUri(event.target.src);
-  }
-
+function App(){
   return (
     <div className="screen-window">
       <div className="launch-screen">
         <div>
           <CameraComponent />
         </div>
-        <div className="rectangle-7">
-          <span className="recomended-size-for-you">
-            Recommended size for you:
-          </span>
-          <span className="s">{clientSize}</span>
+        <div>
+          <SizeChartApp />
         </div>
-        <div className="rectangle">
-          {/* Add click event listener to first rectangle image */}
+        <div className="rectangle-7">
+          <span ></span>
+        </div>
+        <div className="products">
+          <img
+            className="product-1"
+            src="Pictures/5.png"
+            alt=""
+            id="product1"
+  
+          />
+          <div className="flex-container">
+            <span className="choose-product"> CHOOSE PRODUCT</span>
+            <img
+              className="product-3"
+              src="Pictures/6.png"
+              alt=""
+              id="product2"
+            />
+          </div>
+          <img
+            className="product-5"
+            src="Pictures/7.png"
+            alt=""
+            id="product3"
+          />
+        </div>
+      </div>
+      <div className="rectangle">
+
           <img
             className="rectangle-1"
             src="Pictures/1.png"
             alt=""
             id="image1"
-            onClick={handleImageClick}
+    
           />
 
           <div className="flex-container">
-            <span className="choose-brand">From Brands</span>
-            {/* Add click event listener to second rectangle image */}
+        
+
             <img
               className="rectangle-3"
               src="Pictures/2.png"
               alt=""
               id="image2"
-              onClick={handleImageClick}
+  
             />
           </div>
-
-          {/* Add click event listener to third rectangle image */}
           <img
             className="rectangle-5"
             src="Pictures/3.png"
             alt=""
             id="image3"
-            onClick={handleImageClick}
+  
           />
         </div>
       </div>
-      <div className="image-display">
-        {/* Render the selected image in a centered camera component */}
-        {imageUri ? (
-          <CameraComponent>
-            <img src={imageUri} alt="" />
-          </CameraComponent>
-        ) : null}
-      </div>
-    </div>
+    
   );
 }
 
--- src/CameraComponent.js
+++ src/CameraComponent.js
@@ -12,7 +12,7 @@ const CameraComponent = () => {
   });
 
   const handleCameraChange = () => {
-    // Toggle between 'user' and 'environment' modes
+ 
     const newFacingMode =
       videoConstraints.facingMode === "user" ? "environment" : "user";
       setVideoConstraints({ facingMode: newFacingMode });
@@ -37,31 +37,23 @@ const CameraComponent = () => {
         setIsDisabled(false);
         setRemainingTime(5);
   
-        // Capture an image from the webcam and set it as imageData
         const imageSrc = webcamRef.current.getScreenshot();
         setImageData(imageSrc);
   
-        // Load the pre-trained model
         const modelUrl = 'https://storage.googleapis.com/tfjs-models/tfjs/mobilenet_v1_0.25_224/model.json';
         const model = await tf.loadLayersModel(modelUrl);
 
-  
-        // Define inputHeight and inputWidth constants
+   
         const inputHeight = 300;
         const inputWidth = 400;
   
-        // Convert the image to a tensor and resize it to match the input shape expected by the model
         const image = tf.browser.fromPixels(webcamRef.current.video);
         const resizedImage = tf.image.resizeBilinear(image, [inputHeight, inputWidth]);
   
-        // Normalize the pixel values to be between 0 and 1
         const normalizedImage = tf.div(tf.sub(resizedImage, [0]), [255]);
   
-        // Reshape the input to match the shape expected by the model
         const batchedInput = normalizedImage.expandDims();
   
-        // Make a prediction on the input and extract the desired output
-       // Make a prediction on the input and extract the desired output
        const outputPromise = model.predict(batchedInput);
        if (outputPromise instanceof Promise) {
         const output = await outputPromise;
@@ -71,10 +63,8 @@ const CameraComponent = () => {
         processPrediction(output);
       }
       
-        // Update the state with the measurement
         setMeasurement(measurement);
   
-        // Display the measurement in the console
         console.log(`Measurement: ${measurement}`);
       }, 5000);
     } catch (error) {
@@ -83,9 +73,9 @@ const CameraComponent = () => {
   }
   
   function processPrediction(inputs) {
-    const predictedValues = inputs.arraySync()[0]; // Convert the tensor data to an array
-    const maxIndex = predictedValues.indexOf(Math.max(...predictedValues)); // Find the index of the max value
-    return maxIndex; // Return the index of the max value as the prediction
+    const predictedValues = inputs.arraySync()[0]; 
+    const maxIndex = predictedValues.indexOf(Math.max(...predictedValues)); 
+    return maxIndex;
 }
 
 
@@ -103,7 +93,7 @@ const CameraComponent = () => {
         <button onClick={handleCameraChange} variant="dark" className="close-button">
           SIZE
         </button>
-        <Button variant="danger" onClick={handleButtonClick} disabled={isDisabled}>
+        <Button variant="danger" className="SNAP" onClick={handleButtonClick} disabled={isDisabled}>
           {isDisabled ? `SNAP (${remainingTime})` : "SNAP"}
         </Button>
       </div>
